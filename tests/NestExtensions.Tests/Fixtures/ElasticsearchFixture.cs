1f42fa081c440f4a49b81b32ed6f28e05aae0efc
mapogolions <ivanovpavelalex45@gmail.com>
2023-03-18 00:40:06 +0500

    update fixture

---
+++ tests/NestExtensions.Tests/Fixtures/ElasticsearchFixture.cs
@@ -0,0 +1,51 @@
+using Nest;
+using Testcontainers.Elasticsearch;
+
+namespace NestExtensions.Test.Fixtures;
+
+public class ElasticsearchFixture : IDisposable
+{
+    private const int _hostPort = 9200;
+    private const int _containerPort = 9200;
+    private const string _password = "secret";
+
+    private readonly Lazy<ElasticsearchContainer> _container;
+    private readonly Lazy<IElasticClient> _client;
+
+    public ElasticsearchFixture()
+    {
+        _container = new Lazy<ElasticsearchContainer>(ContainerFactory);
+        _client = new Lazy<IElasticClient>(ClientFactory);
+    }
+
+    public ElasticsearchContainer Container => _container.Value;
+
+    private ElasticsearchContainer ContainerFactory()
+    {
+        var container = new ElasticsearchBuilder()
+            .WithPortBinding(_hostPort, _containerPort)
+            .WithPassword(_password)
+            .WithEnvironment("xpack.security.enabled", "false")
+            .Build();
+        container.StartAsync().Wait();
+        return container;
+    }
+
+    public IElasticClient Client =>  _client.Value;
+
+    private IElasticClient ClientFactory()
+    {
+        var settings = new ConnectionSettings(new Uri($"http://localhost:{Container.GetMappedPublicPort(9200)}"))
+            .BasicAuthentication("elastic", _password)
+            .DefaultFieldNameInferrer(p => p);
+        return new ElasticClient(settings);
+    }
+
+    public void Dispose()
+    {
+        Container.StopAsync().Wait();
+    }
+}
+
+[CollectionDefinition(nameof(ElasticsearchCollection))]
+public class ElasticsearchCollection : ICollectionFixture<ElasticsearchFixture> {}
---
+++ tests/NestExtensions.Tests/PointInTimeReaderTests.cs
@@ -0,0 +1,21 @@
+using NestExtensions.Test.Fixtures;
+
+namespace NestExtensions.Tests;
+
+[Collection(nameof(ElasticsearchCollection))]
+public class PointInTimeReaderTests
+{
+    private readonly ElasticsearchFixture _fixture;
+
+    public PointInTimeReaderTests(ElasticsearchFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task Deleteme()
+    {
+        var res = await _fixture.Client.Cluster.HealthAsync();
+        Assert.True(res.IsValid);
+    }
+}
