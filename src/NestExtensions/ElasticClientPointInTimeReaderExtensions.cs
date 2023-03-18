009c64a7eeae74fc932676f7e2a23c8a3afb69af
mapogolions <ivanovpavelalex45@gmail.com>
2023-03-18 15:21:05 +0500

    configure search request

--- src/NestExtensions/ElasticClientPointInTimeReaderExtensions.cs
+++ src/NestExtensions/ElasticClientPointInTimeReaderExtensions.cs
@@ -4,26 +4,40 @@ namespace NestExtensions;
 
 public static class ElasticClientPointInTimeReaderExtensions
 {
-    public static async Task<IPointInTimeReader<TDocument>> PointInTimeReader<TDocument>(this IElasticClient client, PointInTimeReaderOptions options,
+    public static async Task<IPointInTimeReader<TDocument>> PointInTimeReader<TDocument>(
+        this IElasticClient client,
+        PointInTimeReaderOptions options,
+        Func<SearchDescriptor<TDocument>, ISearchRequest>? configure = null,
         CancellationToken cancellation = default) where TDocument : class
     {
         ArgumentNullException.ThrowIfNull(client);
         Time keepAlive = options.KeepAlive;
         var response = await client.OpenPointInTimeAsync(options.IndexName, o => o.KeepAlive(keepAlive.ToString()));
-        return new PointInTimeReader<TDocument>(client, options, pit: response.Id);
+        return new PointInTimeReader<TDocument>(client, options, response.Id, configure);
     }
 
-    public static Task<IPointInTimeReader<TDocument>> PointInTimeReader<TDocument>(this IElasticClient client, string indexName, int size = 10_000, int slices = 1,
+    public static Task<IPointInTimeReader<TDocument>> PointInTimeReader<TDocument>(
+        this IElasticClient client,
+        string indexName,
+        int size = 10_000,
+        int slices = 1,
+        Func<SearchDescriptor<TDocument>, ISearchRequest>? configure = null,
         CancellationToken cancellation = default)
         where TDocument : class
     {
-        return PointInTimeReader<TDocument>(client, new() { IndexName = indexName, Size = size, Slices = slices }, cancellation);
+        return PointInTimeReader<TDocument>(client, new() { IndexName = indexName, Size = size, Slices = slices }, configure, cancellation);
     }
 
-    public static Task<IPointInTimeReader<TDocument>> PointInTimeReader<TDocument>(this IElasticClient client, string indexName, TimeSpan keepAlive, int size = 10_000, int slices = 1,
+    public static Task<IPointInTimeReader<TDocument>> PointInTimeReader<TDocument>(
+        this IElasticClient client,
+        string indexName,
+        TimeSpan keepAlive,
+        int size = 10_000,
+        int slices = 1,
+        Func<SearchDescriptor<TDocument>, ISearchRequest>? configure = null,
         CancellationToken cancellation = default)
         where TDocument : class
     {
-        return PointInTimeReader<TDocument>(client, new() { IndexName = indexName, Size = size, Slices = slices, KeepAlive = keepAlive }, cancellation);
+        return PointInTimeReader<TDocument>(client, new() { IndexName = indexName, Size = size, Slices = slices, KeepAlive = keepAlive }, configure, cancellation);
     }
 }
